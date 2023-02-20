9b2f7a235f7a416b8f2f15c0f6b77161cec7d585
deampsy <deampsy@proton.me>
2023-02-20 11:44:47 -0500

    ppducksoul69

---
+++ program1.py
@@ -0,0 +1,51 @@
+from os import system
+import time
+
+BLACK = "\033[1;31m"
+RED = "\033[1;32m"
+
+name = input(f"{RED} GIVE ME YOUR NAME IF YOU ARE DORIAN THEN SHOW ME YOUR AGE NAME FOR THINGS AND I AM ZONG DONG WONG THE MASTER OF NORTH CORNEA: ")
+racist = input(f"HELLO {name} ZONG WONG WILL ASK WHAT IS YOUR FAVORITE COLOR IF YOU ARE RACIST THEN SAY WHITE IF NOT SAY ZONG: ")
+if racist == "WHITE":
+    print("next question")
+
+    where = input("where do you live? ")
+    if where == "maine":
+        system("figlet hello i am srek i love to bonce my hed")
+        system("open shrek.gif")
+        time.sleep(2)
+        system("open shrek2.gif")
+        system("figlet i will find you and i WILL kill you")
+        time.sleep(2)
+        system("open finger.gif")
+        system("figlet NAME THIS F BOMB")
+        time.sleep(2)
+        system("open scary.gif")
+        system("figlet POOPY")
+    else:
+        print(" no srek fur eo")
+        system("open running_from_bombs.gif")
+        system("figlet run you fool! them bombs gonna git ya")
+        time.sleep(2)
+        system("open clownsplosion.gif")
+        system("figlet time to get clowny")
+        time.sleep(2)
+        system("open last_bomb.gif")
+        system("figlet BOOOOOOOOOOOOOOOOOOOOOM BABYQ!")
+        time.sleep(2)
+        system("SHUTDOWN IMMIMENT PREPARE TO DIE!!!")
+        system("open death.gif")
+        time.sleep(2)
+        #system("shutdown 0")
+elif racist == "BLUE":
+    system("figlet I WILL NOT SETTLE FOR THIS SMURF")
+    system("open cry.gif")
+    time.sleep(2)
+    system("figlet YOU SAW NOTHING BUT ZONG WONG KONG RONG TONG BONG DONG AONG BONG CONG KONG")
+    system("open suck.jpg")
+else:
+    print("_-_")
+    system("figlet ZONG ALERT! ZONG ALERT! ZONG INCOMING")
+    system("aplay -q alarm.wav")
+    time.sleep(1)
+    system("open zong.svg")
